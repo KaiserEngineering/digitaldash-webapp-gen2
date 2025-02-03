fffdabfd19cd6d8c331f1700ea9424e81d6f056b
Craig Kaiser <craig@bestpractical.com>
2025-02-03 11:37:54 -0500

    Initial app scaffolding

---
+++ svelte.config.js
@@ -0,0 +1,42 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://svelte.dev/docs/kit/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://localhost', // External API
+				changeOrigin: true, // For changing the origin of the request
+				rewrite: (path) => path.replace(/^\/api/, '') // Optional: rewrite path if necessary
+			}
+		}
+	},
+
+	kit: {
+		adapter: adapter({
+			precompress: true,
+			strict: false
+		}),
+		output: {
+			bundleStrategy: 'inline'
+		},
+		alias: {
+			'@/*': './src/lib/*',
+			'$schemas/*': './src/schemas/*'
+		},
+		prerender: {
+			entries: ['/'],
+			handleHttpError: ({ message }) => {
+				// otherwise fail the build
+				throw new Error(message);
+			}
+		}
+	}
+};
+
+export default config;
