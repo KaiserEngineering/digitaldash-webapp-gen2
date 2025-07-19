fb86ae920562e57d9a881e0ea8690854173ba298
Craig Kaiser <craig@bestpractical.com>
2025-07-19 13:47:31 -0400

    Exclude routes/api/ routes from prod build

--- front/svelte.config.js
+++ front/svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess({ script: true }),
@@ -11,7 +13,6 @@ const config = {
 			strict: true,
 			fallback: 'index.html'
 		}),
-
 
 		version: {
 			pollInterval: 0
@@ -25,6 +26,11 @@ const config = {
 			'@/*': './src/lib/*',
 			'$schemas/*': './src/schemas/*',
 			'$local/*': './src/local/*'
+		},
+
+		// Exclude API routes in production
+		files: {
+			routes: isDev ? 'src/routes' : 'src/routes/!(api)'
 		}
 	}
 };
