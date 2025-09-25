dd04227ad0f06da38c0f6a416c93e1b8d82c1e3c
Craig Kaiser <craig@kaiserengineering.io>
2025-09-25 16:07:01 -0400

    Create helper method get_spiffs_usage_info

--- main/spiffs_init.h
+++ main/spiffs_init.h
@@ -1,7 +1,11 @@
 #ifndef SPIFFS_INIT_H
 #define SPIFFS_INIT_H
 
+#include "esp_err.h"
+#include <stddef.h>
+
 void init_spiffs(void);
 void list_spiffs_files(void);
+esp_err_t get_spiffs_usage_info(size_t *total, size_t *used);
 
 #endif
