bdcd2bbcdb831cbb0e5243e654d43af90a9b9860
Craig Kaiser <craig@kaiserengineering.io>
2025-10-06 22:15:10 -0400

    Build index

--- components/web_server/include/version.h
+++ components/web_server/include/version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define APP_VERSION_STRING "1758838894675"
-#define VERSION_JSON_RESPONSE "{\"version\": \"1758838894675\"}"
+#define APP_VERSION_STRING "1759802443827"
+#define VERSION_JSON_RESPONSE "{\"version\": \"1759802443827\"}"
 
 #endif
