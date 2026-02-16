862b5b93d5147c7610c421fae67b2dd2b29b2c01
Matt Kaiser <MatthewKaiser45@gmail.com>
2026-02-15 22:02:58 -0500

    Update index.html.gz and bump to v1.0.6

--- components/web_server/include/version.h
+++ components/web_server/include/version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define APP_VERSION_STRING "1770654053579"
-#define VERSION_JSON_RESPONSE "{\"version\": \"1770654053579\"}"
+#define APP_VERSION_STRING "1771210346182"
+#define VERSION_JSON_RESPONSE "{\"version\": \"1771210346182\"}"
 
 #endif
